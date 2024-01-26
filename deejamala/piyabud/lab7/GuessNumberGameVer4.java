48f39d506c9db95f6159822a02182ae13bc28338
Piyabud <piyabud.d@kkumail.com>
2024-01-26 10:03:58 +0700

    update lab7

--- deejamala/piyabud/lab7/GuessNumberGameVer4.java
+++ deejamala/piyabud/lab7/GuessNumberGameVer4.java
@@ -4,6 +4,7 @@ public class GuessNumberGameVer4 extends deejamala.piyabud.lab5.GuessNumberGameV
     public GuessNumberGameVer4(int minNum, int maxNum, int maxTries) {
         super(minNum, maxNum, maxTries);
     }
+
     @Override
     public String toString() {
         return "GuessNumberGameVer4 (min:" + minNum + ", max:" + maxNum + ", max tries:" + maxTries + ")";
